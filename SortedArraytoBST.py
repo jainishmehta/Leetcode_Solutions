f8393484983a708a70ddc8a71918d965e8b9a09c
Jainish Mehta <45054638+jainishmehta@users.noreply.github.com>
2021-05-27 00:19:18 -0400

    Create SortedArraytoBST.py

---
+++ SortedArraytoBST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        amount=len(nums)
+        def check(nums, start, end):
+            if start>end:
+                return None
+            mid = (start+end)//2
+            root=TreeNode(nums[mid])
+            root.left= check(nums, start, mid-1)
+            root.right =check(nums, mid+1, end)
+            return root
+        return check(nums,0, amount-1)
